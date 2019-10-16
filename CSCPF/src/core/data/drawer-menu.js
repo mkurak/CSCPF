6a142b0735d87a6f1c5f0520b5b200131048dea5
Mesut Kurak <mesutkurak@hotmail.com>
2019-10-16 19:37:59 +0300

    2.1.0 - UserManagement menu is managed from project-env.js

--- CSCPF/src/core/data/drawer-menu.js
+++ CSCPF/src/core/data/drawer-menu.js
@@ -1,43 +1,51 @@
 import t from "@/core/plugins/i18n";
 import projectMenu from "@/project/integration/data/drawer-menu";
+import projectEnv from "@/project/integration/project-env";
 
-const menu = [
-  {
-    title: t.t("menu.currentUser.currentUser"),
-    icon: "mdi-account",
-    role: "",
-    roleStatus: true,
-    active: false,
-    url: "/CurrentUser",
-    checkChildRoleForThis: false,
-    children: [
-      {
-        title: t.t("menu.currentUser.profile"),
-        url: "/CurrentUser/Profile",
-        role: "",
-        roleStatus: true
-      },
-      {
-        title: t.t("menu.currentUser.notification"),
-        url: "/notifications",
-        role: "",
-        roleStatus: true
-      },
-      {
-        title: t.t("menu.currentUser.messages"),
-        url: "/messages",
-        role: "",
-        roleStatus: true
-      },
-      {
-        title: t.t("menu.currentUser.logout"),
-        url: "/login",
-        role: "",
-        roleStatus: true
-      }
-    ]
-  },
-  {
+const menu = [];
+
+menu.push({
+  title: t.t("menu.currentUser.currentUser"),
+  icon: "mdi-account",
+  role: "",
+  roleStatus: true,
+  active: false,
+  url: "/CurrentUser",
+  checkChildRoleForThis: false,
+  children: [
+    {
+      title: t.t("menu.currentUser.profile"),
+      url: "/CurrentUser/Profile",
+      role: "",
+      roleStatus: true
+    },
+    {
+      title: t.t("menu.currentUser.notification"),
+      url: "/notifications",
+      role: "",
+      roleStatus: true
+    },
+    {
+      title: t.t("menu.currentUser.messages"),
+      url: "/messages",
+      role: "",
+      roleStatus: true
+    },
+    {
+      title: t.t("menu.currentUser.logout"),
+      url: "/login",
+      role: "",
+      roleStatus: true
+    }
+  ]
+});
+
+let addUMMenu = true;
+if (projectEnv.core && projectEnv.core.layout)
+  addUMMenu = projectEnv.core.layout.viewUserManagementMenus;
+
+if (addUMMenu) {
+  menu.push({
     title: t.t("menu.userManagement.userManagement"),
     icon: "mdi-account-group",
     role: "",
@@ -65,8 +73,9 @@ const menu = [
         roleStatus: true
       }
     ]
-  },
-  ...projectMenu
-];
+  });
+}
 
-export default menu;
+const menu2 = [...menu, ...projectMenu];
+
+export default menu2;
